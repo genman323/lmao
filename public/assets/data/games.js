010cc1952fa9db9fe10c90140c30a638f8e44b8d
genman323 <genman323@gmail.com>
2026-04-15 00:01:58 -0700

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -42,12 +42,6 @@ var games = [
     image: 'https://play-lh.googleusercontent.com/X8FEOENa_t5mlRllBh2RRLc6sMnPIq56Wxa2ndValDOmwzIRhAv7MG6fT97TLQ2DK48'
   },
   {
-    id: 'hidenseek',
-    title: 'Hide N Seek',
-    url: 'https://effortless-mermaid-f8d427.netlify.app/',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKSRcWv9wuD9wT0UN0y5Z_duvXt-ySXG7p7A&s'
-  },
-  {
     id: 'fivenightsatepsteins',
     title: "Five Nights At Epstein's",
     url: 'https://tranquil-sunflower-16a155.netlify.app/',
@@ -112,6 +106,138 @@ var games = [
     title: 'Block Blast',
     url: 'https://peppy-sprinkles-134f15.netlify.app/',
     image: 'https://play-lh.googleusercontent.com/R0qgNDYYHbRhw6JFsdEbDMqONplEvJx0m0W9wzYVvY3eNF1c2rfBWYjQxW0sLEzFe1E=w240-h480-rw'
+  },
+  {
+    id: 'bowmasters',
+    title: 'Bow Masters',
+    url: 'https://unique-bavarois-b4f971.netlify.app/',
+    image: 'https://play-lh.googleusercontent.com/rsQyCuie6S8Munc2Hv6IMpePP1g3c0okLkyKRXd1IimOryIBMZqymGKaOg7bZ6NMkhj3'
+  },
+  {
+    id: 'icedodo',
+    title: 'Ice Dodo',
+    url: 'https://enchanting-kleicha-c00df9.netlify.app',
+    image: 'https://lh3.googleusercontent.com/S7PiewtFMH22v621RHLNrDM3c96Hv112gzBIHLn17QdTpMi-XgyAtZ4FLpLNTBBsBFEtg339hrqKW-eZ8Gl88C95=s1280-w1280-h800'
+  },
+  {
+    id: 'jetpackjoyride',
+    title: 'Jetpack Joyride',
+    url: 'https://harmonious-bonbon-bf5431.netlify.app',
+    image: 'https://upload.wikimedia.org/wikipedia/en/9/90/Jetpack_Joyride_iOS.png'
+  },
+  {
+    id: 'templerun2',
+    title: 'Temple Run 2',
+    url: 'https://peaceful-baklava-adb6f7.netlify.app/',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREczIHVzGHpPjtSgmI-Uq1O6ZJe5eppb711w&s'
+  },
+  {
+    id: 'stickmanhook',
+    title: 'Stickman Hook',
+    url: 'https://ornate-cendol-eadfa5.netlify.app/',
+    image: 'https://m.media-amazon.com/images/I/610klDzmDsL.png'
+  },
+  {
+    id: 'attackhole',
+    title: 'AttackHole',
+    url: 'https://melodious-travesseiro-dbad95.netlify.app/',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTECbaIu1UNxUMhFZqLKevcus8Q18Fyn7hDfQ&s'
+  },
+  {
+    id: 'bridgerace',
+    title: 'Bridge Race',
+    url: 'https://chimerical-cupcake-6545e0.netlify.app/',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR46w5CvNmueL8up1TLIBsM4vMeJqI6zQKn7g&s'
+  },
+  {
+    id: 'magictiles3',
+    title: 'Magic Tiles 3',
+    url: 'https://radiant-beignet-1beb3b.netlify.app/',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtQWmslyDYHqtZMrZhahb9utEaHpWPf_8TFw&s'
+  },
+  {
+    id: 'stackydash',
+    title: 'Stacky Dash',
+    url: 'https://eloquent-lily-f5824e.netlify.app/',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP9TSTl2GdQSCy4EaNGZA2GiW-ZYSceowKiA&s'
+  },
+  {
+    id: 'tallmanrun',
+    title: 'Tall Man Run',
+    url: 'https://fluffy-nasturtium-ad0256.netlify.app',
+    image: 'https://play-lh.googleusercontent.com/-SGcXJAJZJHFuvHiesYY1Df8-Bg_XagSPYgHiYN9g85F75IKgKWRUGCxdROn_VLmW13W=w240-h480-rw'
+  },
+  {
+    id: 'crossyroad',
+    title: 'Crossy Road',
+    url: 'https://hilarious-nougat-e06374.netlify.app',
+    image: 'https://upload.wikimedia.org/wikipedia/en/7/71/Crossy_Road_icon.jpeg'
+  },
+  {
+    id: 'geometrydashlite',
+    title: 'Geometry Dash Lite',
+    url: 'https://steady-kitsune-e5e26a.netlify.app',
+    image: 'https://geometrydash.wiki.gg/images/GeometryDashLite.png?7b9150'
+  },
+  {
+    id: 'basketballfrvr',
+    title: 'Basketball FRVR',
+    url: 'https://teal-narwhal-d75bd9.netlify.app',
+    image: 'https://play-lh.googleusercontent.com/4avPP7yNU6hARlzIldyPajBnKYj3rGohlmfnRtR7JxSGXNbfBW9AblVhY3wAbo0sbYU7'
+  },
+  {
+    id: 'bazookaboy',
+    title: 'Bazooka Boy',
+    url: 'https://chipper-halva-174e09.netlify.app',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTduMnZJJywids1LdIy3ZYS-pekOaHv7HmHXA&s'
+  },
+  {
+    id: 'bottlejump3d',
+    title: 'Bottle Jump 3D',
+    url: 'https://curious-treacle-69325e.netlify.app',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYBjMA2t_WF1IEssj1OPl-mkG3DvoOjD-6qw&s'
+  },
+  {
+    id: 'adanceoffireandice',
+    title: 'A Dance Of Fire and Ice',
+    url: 'https://genuine-naiad-b54474.netlify.app',
+    image: 'https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/store/software/switch/70010000089145/e594a668fd46b661daec1874570cac9d8769b237ddb147bd330b06952b44d267'
+  },
+  {
+    id: 'achievementunlocked',
+    title: 'Achievement Unlocked',
+    url: 'https://aquamarine-kulfi-287ba8.netlify.app',
+    image: 'https://images.launchbox-app.com/4e7fbd64-1b37-4017-a83b-6a4188b11f99.png'
+  },
+  {
+    id: 'achievementunlocked2',
+    title: 'Achievement Unlocked 2',
+    url: 'https://inspiring-zabaione-698b50.netlify.app',
+    image: 'https://images.igdb.com/igdb/image/upload/t_cover_big_2x/co74c7.jpg'
+  },
+  {
+    id: 'achievementunlocked3',
+    title: 'Achievement Unlocked 3',
+    url: 'https://enchanting-pastelito-ba1589.netlify.app',
+    image: 'https://gamemedia.armorgames.com/13196/icn_heroimage.png'
+  },
+  {
+    id: 'angrybirds',
+    title: 'Angry Birds',
+    url: 'https://jovial-pithivier-9b699c.netlify.app',
+    image: 'https://upload.wikimedia.org/wikipedia/en/2/2d/Angry_Birds_promo_art.png'
+  },
+  {
+    id: 'backrooms',
+    title: 'Backrooms',
+    url: 'https://admirable-snickerdoodle-993980.netlify.app',
+    image: 'https://preview.redd.it/what-you-guys-thinking-about-backrooms-v0-sxccvgrnpxfd1.jpeg?width=640&crop=smart&auto=webp&s=f198372f6945a9174ca2f702d28d6d6e6eed9c36'
+  },
+  {
+    id: 'baldisbasics',
+    title: "Baldi's Basics",
+    url: 'https://clinquant-muffin-a08c48.netlify.app/',
+    image: 'https://upload.wikimedia.org/wikipedia/en/1/13/Baldis_Basics_in_Education_and_Learining_cover_art.webp'
   }
 ]
 games.sort(function (a, b) {
